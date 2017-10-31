9e0575de6ebcc81499e4915cee2fccea95f404f1
dmkibuka <dmkibuka@yahoo.com>
2017-10-31 13:22:47 -0400

    Added emailing functionality, now emails will be sent

--- process.php
+++ process.php
@@ -69,7 +69,15 @@ if($_SERVER["REQUEST_METHOD"] == "POST"):
 		if($add_maillist == 1):
 			$sql = "INSERT IGNORE INTO maillist (maillist_email, customer_customer_id) VALUES ('$email','$customer_id')";
 			$db->query($sql);
-		endif;	
+		endif;
+        
+        // Create the email and send the message
+		$to = " "; // Add your email address inbetween the " " replacing yourname@yourdomain.com - This is where the form will send a message to.
+		$email_subject = "Website Contact Form:  {$firstname} {$lastname}";
+		$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nFirst name: $firstname\n\nLast name: $lastname\n\nEmail Address: $email\n\nPhone: $phone\n\nMessage: $message";
+		$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+		$headers .= "Reply-To: $email";   
+		mail($to,$email_subject,$email_body,$headers);	
 
 		if($db->affected_rows()):
              //Success message
